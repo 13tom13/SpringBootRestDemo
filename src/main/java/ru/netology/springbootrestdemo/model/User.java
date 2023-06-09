f8c94afbc4399343fe973965411ede3918f021d6
13tom13 <13tom13@mail.ru>
2023-06-09 20:24:40 +0200

    jakarta.validation

--- src/main/java/ru/netology/springbootrestdemo/model/User.java
+++ src/main/java/ru/netology/springbootrestdemo/model/User.java
@@ -1,8 +1,9 @@
 package ru.netology.springbootrestdemo.model;
 
 
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Size;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 
 public final class User {
     @Size(min = 10, max = 30)
